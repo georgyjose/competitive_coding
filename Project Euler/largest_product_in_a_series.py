b4b8fc10248d97c2baad36dec08bb1cde070200a
Georgy Jose <georgyjose@Georgys-MBP.Home>
2018-08-05 00:29:58 +0530

    gud nyt

---
+++ Project Euler/largest_product_in_a_series.py
@@ -0,0 +1,4 @@
+a = []
+for i in range(20):
+    a.append(list(map(int,input().split())))
+print (a)
--- Project Euler/palindrome_product.py
+++ Project Euler/palindrome_product.py
@@ -2,5 +2,10 @@ def palindrome(n):
     if n == int(str(n)[::-1]):
         return True
     return False
-while True:
-    print(palindrome(int(input()))
+
+for i in range(998001,10000,-1):
+    if(palindrome(i)):
+        for j in range(100,1000):
+            if i%j ==0 and len(str(i//j))==3:
+                print(i,j,i//j)
+                break
