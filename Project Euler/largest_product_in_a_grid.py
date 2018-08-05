9381d67f12e7b6292232430d71e7bb775f1d5105
Georgy Jose <georgyjose@Georgys-MBP.Home>
2018-08-05 17:18:44 +0530

    Largest product downward and sidewise is computed. Diagonal computation left.

--- Project Euler/largest_product_in_a_grid.py
+++ Project Euler/largest_product_in_a_grid.py
@@ -1,7 +1,30 @@
+import numpy
+
 path = "input_for_largest_product_in_a_grid.txt"
 
-b = []
+def downmax(b,m):
+    d = 20-4
+    for j in range(20):
+        for i in range(d):
+            m = max(m,numpy.prod([b[i][j],b[i+1][j],b[i+2][j],b[i+3][j]]))
+    return m
+            
+
+def rightmax(b,m):
+    d = 20-4
+    for i in range(20):
+        for j in range(d):
+            m = max(m,numpy.prod([b[i][j],b[i][j+1],b[i][j+2],b[i][j+3]]))
+    return m
+
+def ondiagonalmax(b,m):
+    pass
+
+def offdiagonalmax(b,m):
+    pass
+    
 
 with open(path,"r") as a:
     b = [list(map(int,i.split())) for i in a]
-
+    
+print(downmax(b,0),rightmax(b,0))
